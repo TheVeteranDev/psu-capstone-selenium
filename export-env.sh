926df8922c1731c5e4fae57ba3b806a459fe98ee
John Rukstalis <theveterandev@gmail.com>
2025-02-09 18:50:37 -0500

    remove env file

--- export-env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-export COGNITO_USERNAME='psucapstonetest'
-export COGNITO_PASSWORD='$Angela050383'
-export COGNITO_EMAIL='psucapstonetest@gmail.com'
-
-echo "Successfully sourced environment variables!"
